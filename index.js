30159e72a40a415b78469fbe519b7eccc5eea680
OkutaniDaichi0106 <132345842+OkutaniDaichi0106@users.noreply.github.com>
2023-11-30 15:23:12 +0900

    test

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 class LIFF{
 	constructor(){
 		console.log("LIFF()...");
-
+		let name;
+		let idToken;
+		const confirmButton = document.getElementById("confirm");
+		const nameForm = document.getElementById("name");
+		let message;
 		liff.init({
 			liffId: "2001929504-zbp7ajrv", 
 			withLoginOnExternalBrowser: true,
 		}).then(() => {
 			//start program
-			let name;
-			let idToken;
-			const confirmButton = document.getElementById("confirm");
-			const nameForm = document.getElementById("name");
-			confirmButton.addEventListener("click", function(){
-				name = nameForm.value;
-				idToken = liff.getIDToken();
-				const message = `名前：${name}、ID：${idToken}`
-				console.log(message);
-				liff.sendMessages([{
-					type: "text", 
-					text: message
-				}]).then(()=>{
-					liff.closeWindow();
-				});
-			});
-			
+			// confirmButton.addEventListener("click", function(){
+			// 	name = nameForm.value;
+			// 	idToken = liff.getIDToken();
+			// 	message = `名前：${name}、ID：${idToken}`
+			// 	console.log(message);
+
+			// 	liff.sendMessages([{
+			// 		type: "text", 
+			// 		text: message
+			// 	}]).then(()=>{
+			// 		//liff.closeWindow();
+			// 	});
+			// });
+
 		}).catch((err) => {
 			console.log(err);
-		})
+		});
+		
+		liff.sendMessages([{
+			type: "text",
+			text: "aaaa"
+		}]).then(()=>{
+			liff.closeWindow();
+		});
 	}
 }
 
