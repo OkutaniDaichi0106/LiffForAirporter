ee0c1a020b3f05c753013752a0a65da02edf4ece
OkutaniDaichi0106 <132345842+OkutaniDaichi0106@users.noreply.github.com>
2023-11-30 15:41:26 +0900

    test

--- index.js
+++ index.js
@@ -10,30 +10,14 @@ class LIFF{
 			liffId: "2001929504-zbp7ajrv", 
 			withLoginOnExternalBrowser: true,
 		}).then(() => {
-			//start program
-			// confirmButton.addEventListener("click", function(){
-			// 	name = nameForm.value;
-			// 	idToken = liff.getIDToken();
-			// 	message = `名前：${name}、ID：${idToken}`
-			// 	console.log(message);
-
-			// 	liff.sendMessages([{
-			// 		type: "text", 
-			// 		text: message
-			// 	}]).then(()=>{
-			// 		//liff.closeWindow();
-			// 	});
-			// });
-
+			liff.sendMessages([{
+				type: "text",
+				text: "aaaa"
+			}]).then(()=>{
+				liff.closeWindow();
+			});
 		}).catch((err) => {
 			console.log(err);
-		});
-		
-		liff.sendMessages([{
-			type: "text",
-			text: "aaaa"
-		}]).then(()=>{
-			liff.closeWindow();
 		});
 	}
 }
