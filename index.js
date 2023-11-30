58c9b0c95d0ed4b826700d91e443d972195b9729
OkutaniDaichi0106 <132345842+OkutaniDaichi0106@users.noreply.github.com>
2023-11-30 12:02:58 +0900

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+const confirmButton = document.getElementById("confirm");
+const nameForm = document.getElementById("name")
+
+class LIFF{
+	constructor(){
+		console.log("LIFF()...");
+
+		liff.init({
+			liffId: "liffId", 
+			withLoginOnExternalBrowser: true,
+		}).then(() => {
+			//start program
+			let name;
+			let idToken;
+			confirmButton.addEventListener("click", ()=>{
+				name = nameForm.value;
+				idToken = new LIFF().getIdToken();
+			})
+			const message = `名前：${name}、ID：${idToken}`
+			liff.sendMessages([
+				{
+					type: "text", 
+					text: message,
+				},
+			]);
+			closeWindow();
+		}).catch((err) => {
+			console.log(err);
+		})
+	}
+	getIdToken(){
+		const idToken = liff.getIDToken();
+		return idToken;
+	}
+}
